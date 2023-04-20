31a5514a764eadeeebf3685d500a80b5f0933532
Manoj4499 <s561637@nwmissouri.edu>
2023-04-19 22:03:25 -0500

    detail.pug

--- controllers/guns.js
+++ controllers/guns.js
@@ -87,3 +87,15 @@ exports.guns_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    exports.guns_view_one_Page = async function(req, res) {
+        console.log("single view for id " + req.query.id)
+        try{
+        result = await guns.findById( req.query.id)
+        res.render('gunsdetail',
+        { title: 'guns Detail', toShow: result });
+        }
+        catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+        }
+        };
