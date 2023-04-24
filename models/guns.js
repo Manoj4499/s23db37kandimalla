7b4c6c66326d919ec26b635c595d0d860646c0ca
Manoj4499 <s561637@nwmissouri.edu>
2023-04-24 00:49:29 -0500

    min max values

--- models/guns.js
+++ models/guns.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const gunsSchema = mongoose.Schema({
-guns_type: String,
-guns_name: String,
-guns_cost: Number
+guns_type: {type: String, minlength:5, maxlenght:30},
+guns_name: {type: String, minlength:5, maxlenght:30},
+guns_cost: {type: Number, minlength:100, maxlenght:2000}
 })
 module.exports = mongoose.model("guns", gunsSchema)
